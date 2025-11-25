6d4d79bdb7e0e2df5068cd54d246a29a22c71fee
crocuses <nicholasodeanjohnson42@gmail.com>
2025-11-25 13:01:10 -0600

    Add files via upload

---
+++ src/1.js
@@ -0,0 +1,24 @@
+
+  function loadImageFromHTML() {
+    fetch('img/1.html')
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById('image-container').innerHTML = html;
+      });
+  }
+
+  if (window.location.hash === '#1') {
+    loadImageFromHTML();
+  }
+
+  window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#1') {
+      loadImageFromHTML();
+    } else {
+      document.getElementById('image-container').innerHTML = ''; // Optional: clear on other hash
+    }
+  });
+
+
+
+
